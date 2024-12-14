10e10759b286a6383cc4bb9f75cbf8bdd7e349e4
MARSE010 <QELLIMISHUMELEK@GMAIL.COM>
2024-12-14 09:53:15 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'kam dicka'.split('')
+const text = 'smth for u'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`

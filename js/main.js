3f10d2706088604c7b4051c10d2b77c4737a5ba6
MARSE010 <QELLIMISHUMELEK@GMAIL.COM>
2024-12-14 09:52:13 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('for you 🌹').split('')
+    const titles = ('per ty<3💐').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
